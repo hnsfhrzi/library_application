2921df066f60dab850a5a63fc018bb8b410da1b2
adinekinar <adinekinari@gmail.com>
2021-12-03 09:06:46 +0700

    Add PreviewPage

---
+++ lib/bottomnavbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return FancyBottomNavigation(
+      activeIconColor: Colors.white,
+      circleColor: Color(0xFFB7CF67),
+      inactiveIconColor: Color(0xFF4F4F4F),
+      tabs: [
+        TabData(iconData: Icons.home, title: "Home"),
+        TabData(iconData: CupertinoIcons.book_solid, title: "List Cerita"),
+        TabData(iconData: Icons.search, title: "Search"),
+        TabData(iconData: CupertinoIcons.bookmark_solid, title: "Bookmark"),
+        //TabData(iconData: Icons.settings, title: "Setting")
+      ],
+      onTabChangedListener: (position) {},
+    );
+  }
+}
